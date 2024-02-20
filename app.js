39abb3be5def1b4b44d265b4078c7be2c9be5b9c
MuhammadAliShaikh0 <ali1104shaikh@gmail.com>
2024-02-20 13:56:17 -0800

    assignment 16

---
+++ app.js
@@ -0,0 +1,116 @@
+// chap 01
+// Q1
+// alert("hello");
+// Q2
+// alert("Error! Please enter a valid password");
+// Q3 
+// alert("Welcome to JS Land... \n Happy Coding!")
+// Q4
+// alert("welcome to JS Land...");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs");
+// Q5
+// alert("Hello... I can run JS through my web browser's console");
+
+// chap 1-3
+
+//  chap 1 alerts
+// Q1
+// alert("firstname");
+// alert("lastname");
+// alert("Email");
+// alert("phonenumber")
+// alert("password");
+// Q2
+// alert("you're learning JavaScript!");
+// Q3
+// alert("my name is ali");
+
+// chap 2 variable for string
+// Q1
+// var name = "MuhammadAli"
+// alert(name);
+// Q2
+// name = 
+// "Muhammad Ali"
+// alert(name);
+// Q3
+// teamName = "Team a";
+// alert(teamName);
+// Q4
+// bestMan = "john doe";
+
+// chap 3 variables for number
+// Q1
+// caseQTY = ""
+// Q2
+// caseQTY = 144
+// Q3
+// var num = 9;
+// document.write(num+1);
+// Q4
+// num = 
+// 0
+// alert(num);
+// Q5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+// Q6
+// number = 10;
+// alert(number + 10);
+
+// chap 02
+// Q1
+//  var username = ""
+// Q2
+// myName = "Muhammad Ali"
+// Q3
+// var message = "Hello World";
+// alert(message);
+// Q4
+// name = "Muhammad Ali"
+// age = 16
+// institute = "Jawan Pakistan"
+// alert(name);
+// alert(age);
+// alert(institute);
+// Q5
+// alert("pizza \n pizz \n piz \n pi \n p")
+// Q6
+// email = " ali1104@gmail.com";
+// alert("My email address is"+ email );
+// Q7
+// book = " A smarter \n way to learn JavaScript";
+// alert("I  am trying to learn from the book" + book);
+// Q8
+// document.write("Yah! I can write HTML content through JavaScript");
+// Q9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// chap 03
+// Q1
+// age = 16
+// alert("I am " + age + " year old")
+// Q2
+// N = 14
+// alert("You have visited this site " +N+ " times");
+// Q3
+// var birthyear = 2007
+// document.write("my birth year is " +birthyear );
+// document.write("Data type of mydeclared variable is "  + typeof birthyear);
+// Q4
+name = "<b>John Doe</b>"
+title = "<b>T-Shirt</b>"
+quantity = "<b>5</b>"
+document.write(name + " ordered " + quantity+ " " + title +"(s) on XYZ clothing store")
+
+
+
+
+
+
+
+
+
+
